6601a5e6a60651709fa1b6be95e167d93d01c7ad
olesama <786gkgf929@privaterelay.appleid.com>
2026-09-24 15:22:56 +0900

    Refresh nationwide mall cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="indoor-nav-station-guidance-v5";
+const CACHE_NAME="indoor-nav-nationwide-malls-v6";
 const FILES=["./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
